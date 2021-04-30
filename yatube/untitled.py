f7a9e4f5fc85bb6d039d24ddb061ef37404b207f
Лёва <netrebaleva@gmail.com>
2021-04-30 13:47:34 +0400

    Fixed

---
+++ yatube/untitled.py
@@ -0,0 +1,50 @@
+import time
+from functools import wraps
+
+
+def time_check(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        execution_time = round(time.time() - start_time, 1)
+        print(f'Время выполнения функции {func.__name__}: {execution_time} с.')
+        return result
+
+    return wrapper
+
+
+def cache_args(func):
+	dicts = {}
+	@wraps(func)
+	def wrapp(a):
+		if a in dicts:
+			return dicts[a]
+		else:
+			result = func(a) 
+			dicts.update({a: result})
+			return result
+	return wrapp
+            
+@time_check
+@cache_args
+def long_heavy(num):
+    time.sleep(1)
+    return num * 2
+
+
+print(long_heavy(1))
+# Время выполнения функции long_heavy: 1.0 с.
+# 2
+print(long_heavy(1))
+# Время выполнения функции long_heavy: 0.0 с.
+# 2
+print(long_heavy(2))
+# Время выполнения функции long_heavy: 1.0 с.
+# 4
+print(long_heavy(2))
+# Время выполнения функции long_heavy: 0.0 с.
+# 4
+print(long_heavy(2))
+# Время выполнения функции long_heavy: 0.0 с.
+# 4
